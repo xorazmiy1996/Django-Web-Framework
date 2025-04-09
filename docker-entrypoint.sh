0281453b6892301353b3fa111f0dda0a80b36a58
xorazmiy1996 <muhammadibragimov439@gmail.com>
2025-04-09 12:07:38 +0500

    edit settings.py

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@
 mkdir -p staticfiles && chmod -R 777 staticfiles
 
 # Statik fayllarni yig'ish
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 
 # Migratsiyalarni yaratish va qo'llash
 python manage.py makemigrations
