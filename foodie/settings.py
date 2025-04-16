a7888050767f12042f45381c7de121f5d43b1d14
xorazmiy1996 <muhammadibragimov439@gmail.com>
2025-04-16 12:32:06 +0500

    edit README.md

--- foodie/settings.py
+++ foodie/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-j*1xcix4_)kspj1-r&5thb*6-p%e3qk!urdb+v6q#0=mw1#k+3
 DEBUG = False
 
 # ALLOWED_HOSTS = ['185.188.183.243','test-uchun.uz','www.test-uchun.uz']
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','*']
 
 
 
@@ -87,25 +87,25 @@ WSGI_APPLICATION = 'foodie.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 # .env faylini yuklash
 load_dotenv()
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('POSTGRES_DB'),             # Yaratilgan ma'lumotlar bazasi nomi
-#         'USER': os.getenv('POSTGRES_USER'),           # Yaratilgan foydalanuvchi nomi
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),   # Foydalanuvchining paroli
-#         'HOST': 'db',      # Agar PostgreSQL lokal serverda bo'lsa
-#         'PORT': '5432',    # Standart port 5432
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),             # Yaratilgan ma'lumotlar bazasi nomi
+        'USER': os.getenv('POSTGRES_USER'),           # Yaratilgan foydalanuvchi nomi
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),   # Foydalanuvchining paroli
+        'HOST': 'db',      # Agar PostgreSQL lokal serverda bo'lsa
+        'PORT': '5432',    # Standart port 5432
+    }
+}
 
 
 # Password validation
