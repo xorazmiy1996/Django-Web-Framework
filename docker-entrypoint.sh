37cdabaeaaba119728890cd832040908701d1206
xorazmiy1996 <muhammadibragimov439@gmail.com>
2025-04-17 12:22:09 +0500

    edit line separator in Dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,1 @@
-#!/bin/sh
-
-# Kerakli joylarni tayyorlash
-#mkdir -p staticfiles && chmod -R 777 staticfiles
-
-# Statik fayllarni yig'ish
-python manage.py collectstatic --no-input
-
-# Migratsiyalarni yaratish va qo'llash
-python manage.py migrate
-
-# Gunicorn orqali Django ilovasini ishga tushirish
-exec gunicorn --bind 0.0.0.0:8000 foodie.wsgi:application
-
+#!/bin/sh# Kerakli joylarni tayyorlash#mkdir -p staticfiles && chmod -R 777 staticfiles# Statik fayllarni yig'ishpython manage.py collectstatic --no-input# Migratsiyalarni yaratish va qo'llashpython manage.py makemigrationspython manage.py migrate# Gunicorn orqali Django ilovasini ishga tushirishexec gunicorn --bind 0.0.0.0:8000 foodie.wsgi:application
